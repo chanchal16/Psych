b7995c2decab18142096010b317686e25d1289a7
chanchal16 <rajputchanchal17@gmail.com>
2022-03-28 13:02:52 +0530

    update - add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import {Navbar,Footer} from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Navbar/>
+      <main></main>
+      <Footer/>
     </div>
   )
 }
