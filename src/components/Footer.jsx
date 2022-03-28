09338a8532f8a37158dfe36f2c61dfe2177273d2
chanchal16 <rajputchanchal17@gmail.com>
2022-03-28 13:03:02 +0530

    Create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/footer.css'
+
+export function Footer() {
+  return (
+    <div>
+        <footer className="footer">
+            <p className="text-md footer-header">made by &nbsp;<span>Chanchal</span></p>
+            <div className="socials">
+            <a className="links" href="https://github.com/chanchal16" target="_blank">
+                <i className="fab fa-github fa-2x"></i></a>
+            <a className="links" href="https://twitter.com/chanchal16_" target="_blank" >
+                <i className="fab fa-twitter fa-2x"></i></a>
+            <a className="links" href="https://www.linkedin.com/in/rajput07/" target="_blank">
+                <i className="fab fa-linkedin-in fa-2x"></i></a>
+            </div>
+            <p className="text-xs footer-header">© 2022 &nbsp;<span>psych!</span></p>
+        </footer>
+    </div>
+  )
+}
+
