9c6ee79b642b7500c1b44d3433a13591c7117166
chanchal16 <rajputchanchal17@gmail.com>
2022-03-29 17:10:41 +0530

    Update - changed ids

--- src/Utils/categories-data.js
+++ src/Utils/categories-data.js
@@ -1,26 +1,26 @@
 const categories = [
     {
-        id:1,
+        id:9,
         name:'General Knowledge',
         img:'https://www.iqueideas.in/wp-content/uploads/2019/08/General-Knowledge-and-Current-Affairs.jpg',
     },
     {
-        id:2,
-        name:'Computer Science',
-        img:'https://images4.content-hci.com/commimg/myhotcourses/blog/post/myhc_89683.jpg'
-    },
-    {
-        id:3,
-        name:'Sports',
-        img:'https://www.nct.ac.in/assets/img/sports/slider/1.jpg'
-    },
-    {
-        id:4,
+        id:10,
         name:'Books',
         img:'https://media.istockphoto.com/photos/flying-color-books-on-pastel-yellow-background-picture-id1304915362?b=1&k=20&m=1304915362&s=170667a&w=0&h=1oBLMT9JLYt6Ju3LbSppu8Fga92YfvSHiPu7zQlculg='
     },
     {
-        id:5,
+        id:18,
+        name:'Computer Science',
+        img:'https://images4.content-hci.com/commimg/myhotcourses/blog/post/myhc_89683.jpg'
+    },
+    {
+        id:21,
+        name:'Sports',
+        img:'https://www.nct.ac.in/assets/img/sports/slider/1.jpg'
+    },
+    {
+        id:31,
         name:'Japanese Anime & Manga',
         img:'https://api.time.com/wp-content/uploads/2020/12/best-anime-series-on-netflix-2.jpg'
     }
