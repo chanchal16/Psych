96b6f6c7d2f213346bf53783713ea9ae0530ed33
chanchal16 <rajputchanchal17@gmail.com>
2022-04-01 23:43:22 +0530

    Update - handle score and index cases

--- src/reducers/questionsReducer.js
+++ src/reducers/questionsReducer.js
@@ -1,7 +1,19 @@
 const questionsReducer = (state,{type,payload})=>{
     switch(type){
         case 'GET_QUESTIONS':
-            return {...state,questions:payload}
+            return {...state,questions:payload};
+        case 'GET_INDEX':
+            return {
+                ...state,
+                selectedOption:'',
+                index:state.index <= 9 && state.index +1
+            };
+        case 'GET_SCORE':
+            return {
+                ...state,
+                selectedOption:payload,
+                score:payload === state.questions[state.index]?.correct_answer ? state.score + 10 :state.score
+            };
         default:
             return state
     }
