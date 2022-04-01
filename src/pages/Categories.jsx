3f133bec1b8c46990c5ed08b745b0496bf1b2bdb
chanchal16 <rajputchanchal17@gmail.com>
2022-04-01 23:41:42 +0530

    update - add fetch questions handler

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import { useQuestions } from '../contexts/QuestionsContext';
 import '../styles/categories.css';
 import { categories } from '../Utils/categories-data';
 
+
 export function Categories() {
-   
+  const {questionsState,categoryId,fetchQuestionsHandler} = useQuestions()
+
+  useEffect(async() => {
+    await fetchQuestionsHandler(categoryId)
+    console.log('questionstate',questionsState)
+    
+  }, [categoryId])
+
   return (
     <div >
         <h1 className='center-text h5'>Select by Category</h1>
         <div className='categories-container'>
         {
             categories?.map(category=>(   
-              <Link to='rules'>           
-              <div class="card category-card" key={category.id}>
-                <div class="card-media">
-                    <img class="vc-image"
-                    src={category.img}
-                    alt="cover" />
-                </div>
-                <div class="overlay">
-                    <p class="overlay-text text-sm">{category.name}</p>
-                </div>                    
-              </div> 
+              <Link to='questions' key={category.id}>           
+                <div class="card category-card"  onClick={()=>fetchQuestionsHandler(category.id)}>
+                  <div class="card-media">
+                      <img class="vc-image"
+                      src={category.img}
+                      alt="cover" />
+                  </div>
+                  <div class="overlay">
+                      <p class="overlay-text text-sm">{category.name}</p>
+                  </div>                    
+                </div> 
               </Link> 
             ))
         }
@@ -29,3 +38,4 @@ export function Categories() {
     </div>
   )
 }
+
