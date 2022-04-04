f7345f25d337c530d28e2bf1bf07540d3735b304
chanchal16 <rajputchanchal17@gmail.com>
2022-04-04 19:25:18 +0530

    Update - add description property

--- src/Utils/categories-data.js
+++ src/Utils/categories-data.js
@@ -2,26 +2,64 @@ const categories = [
     {
         id:9,
         name:'General Knowledge',
+        mostplayed:false,
+        description:'Come along and test your general knowledge.',
         img:'https://www.iqueideas.in/wp-content/uploads/2019/08/General-Knowledge-and-Current-Affairs.jpg',
     },
     {
         id:10,
         name:'Books',
+        mostplayed:false,
+        description:'Come along and test your knowledge in books.',
         img:'https://media.istockphoto.com/photos/flying-color-books-on-pastel-yellow-background-picture-id1304915362?b=1&k=20&m=1304915362&s=170667a&w=0&h=1oBLMT9JLYt6Ju3LbSppu8Fga92YfvSHiPu7zQlculg='
+    },
+    {
+        id:14,
+        name:'Television',
+        mostplayed:true,
+        description:'Come along and test your television knowledge.',
+        img:'https://ia801902.us.archive.org/33/items/mzl.tdczzcbz1/series/15700070751_88d83d38fd_o.png'
+    },
+    {
+        id:15,
+        name:'Video Games',
+        mostplayed:true,
+        description:'Come along and test your video games knowledge.',
+        img:'https://i.guim.co.uk/img/media/7c2ab1a3e60e445caf0a4d3de302591e830e8f7f/0_0_3800_2280/master/3800.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49331bc571155fab736c3ce179f31770'
     },
     {
         id:18,
         name:'Computer Science',
+        mostplayed:false,
+        description:'Come along and test your knowledge in computers.',
         img:'https://images4.content-hci.com/commimg/myhotcourses/blog/post/myhc_89683.jpg'
     },
     {
         id:21,
         name:'Sports',
+        mostplayed:false,
+        description:'Come along and test your sports knowledge.',
         img:'https://www.nct.ac.in/assets/img/sports/slider/1.jpg'
     },
     {
+        id:22,
+        name:'Geography',
+        mostplayed:false,
+        description:'Come along and test your geography knowledge.',
+        img:'https://images.unsplash.com/photo-1521295121783-8a321d551ad2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2VvZ3JhcGh5fGVufDB8fDB8fA%3D%3D&w=1000&q=80'
+    },
+    {
+        id:30,
+        name:'Gadgets',
+        mostplayed:true,
+        description:'Come along and test your knowledge in gadgets.',
+        img:'https://5.imimg.com/data5/SELLER/Default/2021/4/US/FX/DC/119904249/electronic-gadgets-500x500.jpg'
+    },
+    {
         id:31,
+        mostplayed:false,
         name:'Japanese Anime & Manga',
+        description:'Come along and test your animes knowledge.',
         img:'https://api.time.com/wp-content/uploads/2020/12/best-anime-series-on-netflix-2.jpg'
     }
 ]
