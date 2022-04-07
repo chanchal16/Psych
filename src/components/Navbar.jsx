cce09268869701c504e0bfba4c2fe170953bf1aa
chanchal16 <rajputchanchal17@gmail.com>
2022-04-07 23:59:03 +0530

    chore - add rules link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,11 @@ export function Navbar() {
             <Link to='/'  className="brand-name h6">
                 Psych!
             </Link>
+            <nav>
+              <Link to='/rules' class="secondary-link">
+                Guidelines
+              </Link>
+            </nav>
         </header>
 
     </div>
