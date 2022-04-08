5cc94c10cc938de9b9b894421ce83a9e37d204c8
chanchal16 <rajputchanchal17@gmail.com>
2022-04-08 11:38:01 +0530

    chore - add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/nav.css'
+import '../styles/nav.css';
+import spikes from '../../assets/spikes.svg'
 
 export function Navbar() {
   return (
     <div>
         <header class="navbars">
-            <Link to='/'  className="brand-name h6">
-                Psych!
+          <Link to=''  className="menu-icon">
+            <img src={spikes} width="40px" height="40px" alt="logo" />
+          </Link>
+          <Link to='/'  className="brand-name h6">
+              Psych!
+          </Link>
+          <nav>
+            <li className="list-items">
+            <Link to='/rules' class="secondary-link">
+              Guidelines
             </Link>
-            <nav>
-              <Link to='/rules' class="secondary-link">
-                Guidelines
-              </Link>
-            </nav>
+            </li>
+          </nav>
         </header>
 
     </div>
