2e372456b52ac12512f11041e3a828e9c242fe01
chanchal16 <rajputchanchal17@gmail.com>
2022-04-08 19:44:25 +0530

    chore - add back button

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { CategoryCard } from '../components';
 import { categories } from '../Utils/categories-data';
+import { useNavigate } from "react-router-dom";
 
 export function Categories() {
+  let navigate = useNavigate();
   return (
     <div >
         <h1 className='center-text h5'>Select by Category</h1>
+        <button class="button fab-primary" onClick={()=>navigate('/')}>
+          <i class="fas fa-arrow-left"></i>
+        </button>
         <div className='categories-container'>
         {
             categories?.map(category=>(             
