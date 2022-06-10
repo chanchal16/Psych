498380db2f8970832ebae669a3085774bfeab8cb
chanchal16 <rajputchanchal17@gmail.com>
2022-06-10 15:00:50 +0530

    Update - add clear search bar

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,9 +1,10 @@
 import React,{useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import { useQuestions } from '../contexts/QuestionsContext';
+import { Link,useNavigate } from 'react-router-dom';
+import { useQuiz } from '../contexts/QuizContext';
 
 export function CategoryCard({id,name,img,description}) {
-  const {categoryId,fetchQuestionsHandler} = useQuestions()
+  const navigate = useNavigate()
+  const {categoryId,fetchQuestionsHandler,quizDispatch} = useQuiz()
 
   useEffect(async() => {
     await fetchQuestionsHandler(categoryId)  
@@ -22,7 +23,7 @@ export function CategoryCard({id,name,img,description}) {
               </div>
               <div className='desc'>
                 <span>{description}</span>
-                <Link to='/questions'><button className='button play-btn'>Play</button></Link>
+                <Link to='/questions'><button onClick={()=>quizDispatch({type:'CLEAR'})} className='button play-btn'>Play</button></Link>
               </div>
             </div>                   
         </div> 
