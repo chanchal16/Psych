04a070c2a7daeadb9af899f28444685c3dc797c6
chanchal16 <rajputchanchal17@gmail.com>
2022-06-10 15:01:14 +0530

    Update - add debounce to search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React,{useMemo} from 'react';
+import { Link,useNavigate } from 'react-router-dom';
 import '../styles/nav.css';
 import spikes from '../../assets/spikes.svg'
+import { useQuiz } from '../contexts/QuizContext';
+import { debounce } from '../Utils/debounce';
 
 export function Navbar() {
+  const {quizState,quizDispatch} = useQuiz()
+  const navigate = useNavigate()
+
+  const handleSearch = (e)=>{
+    e.preventDefault()
+    navigate('/categories') 
+  }
+
+  const searchQuizByCategory = (e)=>{
+    quizDispatch({type:'SEARCH',payload:e.target.value.toLowerCase()})    
+  }
+   // debounce onchange handler
+  // memoizes the debounced handler, but also calls debounce() only during initial rendering of the component 
+  const debouncedChangeHandler = useMemo(
+    () => debounce(searchQuizByCategory,1000),
+  [quizState.searchQuery]);
+
   return (
     <div>
         <header class="navbars">
@@ -14,6 +33,11 @@ export function Navbar() {
               Psych!
           </Link>
           <nav>
+            <form onSubmit={handleSearch} className='search-form'>
+              <input type='search' className='search' placeholder='search'
+                onChange={debouncedChangeHandler} />
+              <span className='search-icon gray'><i class="fas fa-search fa-sm"></i> </span>
+            </form>
             <li className="list-items">
             <Link to='/rules' class="secondary-link">
               Guidelines
@@ -21,7 +45,6 @@ export function Navbar() {
             </li>
           </nav>
         </header>
-
     </div>
   )
 }
