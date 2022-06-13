a5450d68f1237fb99c5f2266679cd24ab24f5138
chanchal16 <rajputchanchal17@gmail.com>
2022-06-13 23:47:54 +0530

    Update - add auth context provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import { QuizContextProvider } from './contexts/QuizContext';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <QuizContextProvider>
-      <App />
-      </QuizContextProvider>
+      <AuthContextProvider>
+        <QuizContextProvider>
+          <App />
+        </QuizContextProvider>
+      </AuthContextProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
