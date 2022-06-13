787a434cceaaa9acb29ac95ce7a302c1b766657c
chanchal16 <rajputchanchal17@gmail.com>
2022-06-13 23:48:10 +0530

    chore- export pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,5 @@ export {Rules} from './Rules';
 export {Questions} from './Questions';
 export {Results} from './Results';
 export {PageNotFound} from './PageNotFound';
+export {Login} from './Login';
+export {Signup} from './Signup'
